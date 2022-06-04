df0a424ca057a0ee1e8ce8b8f151c8e6fc5c8188
Abet Kalingga Wijaya <abet.kalingga@ti.ukdw.ac.id>
2022-06-04 23:14:46 +0700

    ADD LOGIN-STYLE

--- login.php
+++ login.php
@@ -1,98 +1,69 @@
 <?php
 session_start();
 
-if (isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $user = $_POST["user"];
     $pass = $_POST["password"];
-    if ($user == 'admin' && $pass == 'admin1'){
+    if ($user == 'admin' && $pass == 'admin1') {
         $_SESSION['username'] = $user;
         header("Location: home.php");
     }
-
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="login-style.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
-    <style>
-        table.center {
-        margin-left: auto; 
-        margin-right: auto;
-        }
-        body {
-            background-image: url(pic/background.png);
-            background-repeat: no-repeat;
-            background-size: cover;
-            display: block;
-        }
-    </style>
-    <title>Document</title>
+    <title>LOGIN</title>
 </head>
-<body class="home-page">
-    <header class="nav">
-        <div class="topBar">
-            <a href="home.php"><img src="pic/home button.png" alt="home icon"></a>
-            <h1 id="webname">FULL SEHAT MAZZEHHH</h1>
-        </div>
-    </header>
+<form id="login-form" action="login.php" method="post">
+    <h2>LOGIN</h2>
+    <br>
+    <label>Username</label>
+    <input class="login-box" type="text" name="user"><br>
+    <label>Password</label>
+    <input class="login-box" type="password" name="password" id="password">
+    <p id="warning-text">Caps Lock is ON</p>
+    <input class="show" id="show-pass" type="checkbox" onclick="myFunction()">Show Password <br>
 
+    <button class="login-button" type="submit" name="submit">Login</button>
+</form>
 
-    <main class="detailss">
-        <div>
-            <h1>Welcome to FULL SEHAT</h1>
-            <article>"Ini adalah sebuah web olahragawan sejati..</article>
-            <article>bagi kalian kaum rebahan, jangan akses website ini :)"</article>
-        </div>
-        <br>
-        <div class="kolom-pilihan">
-            <h2>Tipe Olahraga</h2>
-            <br>
-            
-        </div>
-        <br>
-        <br>
-        <form action="login.php" method="post">
-            <table class="center">
-                <tr>
-                    <td>Username</td>
-                    <td class="jarak"></td>
-                    <td><input class="login-box" type="text" name="user"></td>
-                </tr>
-                <tr><td colspan="3">
-                    <br>
-                    <br>
-                    <br>
-                    
-                </td></tr>
-                <tr>
-                    <td>password</td>
-                    <td class="jarak"></td>
-                    <td><input class="login-box" type="password" name="password"></td>
-                </tr>
-                
-                <tr>
-                    <td colspan="3">
-                        <button type="submit" name="submit">Login</button>
-                    </td>
-                    
-                </tr>
-                
-            </table>
-            
-            
-            
-        </form>
-    </main>
-
-    <footer>
-        <p class="CR">Copyright &copy; Fandy Abet Maxim</p>
-    </footer>
-    
-    
+<div class="footer">
+    Copyright &copy; Fandy Abet Maxim
+</div>
 </body>
+
+<script>
+    function myFunction() {
+        var x = document.getElementById("password");
+        if (x.type === "password") {
+            x.type = "text";
+        } else {
+            x.type = "password";
+        }
+    }
+
+    var input = document.getElementById("password");
+
+    // Get the warning text
+    var text = document.getElementById("warning-text");
+
+    // When the user presses any key on the keyboard, run the function
+    input.addEventListener("keyup", function(event) {
+
+        // If "caps lock" is pressed, display the warning text
+        if (event.getModifierState("CapsLock")) {
+            text.style.display = "block";
+        } else {
+            text.style.display = "none"
+        }
+    });
+</script>
+
 </html>
